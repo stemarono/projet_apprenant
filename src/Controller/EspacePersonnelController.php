a2ce8f8c1a90fb45e801f1ec493032e27161214a
stemarono <stephanielacaze93@gmail.com>
2023-02-16 14:56:10 +0100

    mettre les données des informations personnelles dans l'espace personnel et rajout d'une fonction dans espacePersonnelController

--- src/Controller/EspacePersonnelController.php
+++ src/Controller/EspacePersonnelController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,28 +14,27 @@ use Symfony\Component\Routing\Annotation\Route;
 class EspacePersonnelController extends AbstractController
 {
     #[Route('/espace/personnel', name: 'app_espace_personnel')]
-    public function index(EntityManagerInterface $em,$id=0): Response
+    public function index(UserRepository $userRepository,$id=0): Response
     {
-        $user=$em->getRepository(User::class)->find($id);
-
-      
         
         return $this->render('espace_personnel/index.html.twig', [
         
-          'user'=>$user,
+          'users'=>$userRepository->findAll(),
         ]);
     }
 
 
     #[Route('/espace/personnel/show/{id}', name: 'app_espace_personnel_show')]
-    public function show(EntityManagerInterface $em,$id): Response
+    public function show(EntityManagerInterface $em, $id=0): Response
     {
-       
-        $espace=$em->getRepository(PreInscription::class)->findOneBy($id);
-
-
+        $user=$em->getRepository(User::class)->find($id);
+       if(!$user)
+       {
+            $this->addFlash(type:"error", message:"cet utilisateur n'existe pas");
+            return $this->redirectToRoute('app_espace_personnel_show');
+       }
         return $this->render('espace_personnel/Espace_preinscription.html.twig', [
-           'espace'=>$espace,
+           'user'=>$user,
         ]);
     }
 
