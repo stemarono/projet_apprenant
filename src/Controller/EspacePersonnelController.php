0026e811b1c4eac17c10098be5cc2038844241b6
stemarono <stephanielacaze93@gmail.com>
2023-02-21 14:53:34 +0100

    mise en place du bouton modifier et supprimer dela vue espace-personnel-show

--- src/Controller/EspacePersonnelController.php
+++ src/Controller/EspacePersonnelController.php
@@ -5,7 +5,7 @@ namespace App\Controller;
 use App\Entity\PreInscription;
 use App\Entity\User;
 use App\Form\PreInscriptionType;
-use App\Form\UserType;
+use App\Repository\PreInscriptionRepository;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,7 +18,7 @@ class EspacePersonnelController extends AbstractController
     #[Route('/espace/personnel', name: 'app_espace_personnel')]
     public function index(UserRepository $userRepository): Response
     {
-        
+      
         return $this->render('espace_personnel/index.html.twig', [
         
           'users'=>$userRepository->findAll(),
@@ -54,15 +54,58 @@ class EspacePersonnelController extends AbstractController
     }
 
     #[Route('/espace/personnel/editFolder', name: 'app_espace_personnel_editFolder')]
-    public function editFolder(EntityManagerInterface $em): Response
+    public function editFolder(EntityManagerInterface $em,Request $request,PreInscription $preInscription=null): Response
     {
         $user=$this->getUser();
-           $preInscription= $em->getRepository(PreInscription::class)->findOneBy(['user'=>$user]);
+        $preInscription= $em->getRepository(PreInscription::class)->findOneBy(['user'=>$user]);
+        
+        $form=$this->createForm(PreInscriptionType::class,$preInscription);
+        $form->handleRequest($request);
+        
+       if($form->isSubmitted() && $form->isValid())
+       {
+        
+        $em->persist($preInscription);
+        $em->flush();
+       
+        return $this->redirectToRoute('app_espace_personnel_show');
+       }
+
       
-        return $this->render('pre_inscription/edit.html.twig', [
-           'user'=>$user,
-           'pre_inscription'=>$preInscription,
+        return $this->renderForm('pre_inscription/edit.html.twig', [
+          
+          'pre_inscription'=>$preInscription,
+          'form'=>$form,
         ]);
     }
+
+
+    // #[Route('/espace/personnel/delete/{id}', name: 'app_espace_personnel_delete', methods: ['POST'])]
+    // public function delete(Request $request, PreInscription $preInscription, PreInscriptionRepository $preInscriptionRepository): Response
+    // {
+    //     if ($this->isCsrfTokenValid('delete'.$preInscription->getId(), $request->request->get('_token'))) {
+    //         $preInscriptionRepository->remove($preInscription, true);
+    //     }
+
+    //     return $this->redirectToRoute('app_espace_personnel', [], Response::HTTP_SEE_OTHER);
+    // }
+
+
+    #[Route('/espace/personnel/deleteFolder', name: 'app_espace_personnel_deleteFolder')]
+    public function deleteFolder(EntityManagerInterface $em,Request $request,PreInscriptionRepository $preInscriptionRepository): Response
+    {
+        $user=$this->getUser();
+        $preInscription= $em->getRepository(PreInscription::class)->findOneBy(['user'=>$user]);
+      
+        if($preInscription)
+         {
+            $em->remove($preInscription);
+             $em->flush();  
+             return $this->redirectToRoute('app_espace_personnel');
+         }
+         return $this->renderForm('pre_inscription/_delete_form.html.twig');
+        
+    }
+
 
 }
