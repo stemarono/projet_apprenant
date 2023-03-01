352bcd084e21558fb66fa07d462637f64680b65c
MathildePDB <mathilde.pdb@laposte.net>
2023-03-01 09:48:59 +0100

    excel conversion

--- migrations/Version20230301071928.php
+++ migrations/Version20230301071928.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20230228155838 extends AbstractMigration
+final class Version20230301071928 extends AbstractMigration
 {
     public function getDescription(): string
     {
