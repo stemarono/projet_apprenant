96ebedec3106eb5672fe8879850889bae89d5f55
stemarono <stephanielacaze93@gmail.com>
2023-03-01 21:08:58 +0100

    rajout de la function popupCenter + install ckeditor et assets

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,89 +1,104 @@
 // on va chercher les différents éléments de la page
-const pages = document.querySelectorAll(".page")
-const header = document.querySelector(".header")
-const nbPages = pages.length // nombre de pages du formulaire
-let pageActive = 1
+const pages = document.querySelectorAll(".page");
+const header = document.querySelector(".header");
+const nbPages = pages.length; // nombre de pages du formulaire
+let pageActive = 1;
 
 // on attend le chargement de la page
 window.onload = () => {
-    // on affiche la premiere page du formulaire
-    document.querySelector(".page").style.display = "initial"
+  // on affiche la premiere page du formulaire
+  document.querySelector(".page").style.display = "initial";
 
-    // on affiche les numéros des pages dans l'en-tête
-    // on parcourt la liste des pages
-    pages.forEach((page, index) => {
-        // on crée l'élément "numéro de page"
-        let element = document.createElement("div")
-        element.classList.add("page-num")
-        element.id = "num" + (index + 1)
+  // on affiche les numéros des pages dans l'en-tête
+  // on parcourt la liste des pages
+  pages.forEach((page, index) => {
+    // on crée l'élément "numéro de page"
+    let element = document.createElement("div");
+    element.classList.add("page-num");
+    element.id = "num" + (index + 1);
 
-        if(pageActive === index + 1) {
-            element.classList.add("active")
-        }
-        element.innerHTML = index + 1
-        header.appendChild(element)
-    })
-
-    // on gère les boutons "suivant"
-    let boutons = document.querySelectorAll(".next")
-
-    for(let bouton of boutons){
-        bouton.addEventListener("click", pageSuivante)
+    if (pageActive === index + 1) {
+      element.classList.add("active");
     }
+    element.innerHTML = index + 1;
+    header.appendChild(element);
+  });
 
-    // on gère les boutons "précédent"
-    boutons = document.querySelectorAll(".prev")
+  // on gère les boutons "suivant"
+  let boutons = document.querySelectorAll(".next");
 
-    for (let bouton of boutons){
-        bouton.addEventListener("click", pagePrecedente)
-    }
-}
+  for (let bouton of boutons) {
+    bouton.addEventListener("click", pageSuivante);
+  }
+
+  // on gère les boutons "précédent"
+  boutons = document.querySelectorAll(".prev");
+
+  for (let bouton of boutons) {
+    bouton.addEventListener("click", pagePrecedente);
+  }
+};
 
 // on crée la fonction qui fait avancer le formulaire d'une page
 function pageSuivante() {
-   
-    // on masque toutes les pages
-    for(let page of pages) {
-        page.style.display = "none"
-    }
+  // on masque toutes les pages
+  for (let page of pages) {
+    page.style.display = "none";
+  }
 
-    // on affiche la page suivante
-    this.parentElement.parentElement.nextElementSibling.style.display = "initial"
+  // on affiche la page suivante
+  this.parentElement.parentElement.nextElementSibling.style.display = "initial";
 
-    // on "désactive" la page active
-    document.querySelector(".active").classList.remove("active")
-     
-    // on incrémente pageActive
-     pageActive++
+  // on "désactive" la page active
+  document.querySelector(".active").classList.remove("active");
 
-     // on "active" le noueau numéro
-     document.querySelector("#num"+pageActive).classList.add("active")
+  // on incrémente pageActive
+  pageActive++;
 
+  // on "active" le noueau numéro
+  document.querySelector("#num" + pageActive).classList.add("active");
 }
 
 // on crée la fonction qui fait reculer le formulaire d'une page
 function pagePrecedente() {
-   
-    // on masque toutes les pages
-    for(let page of pages) {
-        page.style.display = "none"
-    }
+  // on masque toutes les pages
+  for (let page of pages) {
+    page.style.display = "none";
+  }
 
-    // on affiche la page suivante
-    this.parentElement.parentElement.previousElementSibling.style.display = "initial"
+  // on affiche la page suivante
+  this.parentElement.parentElement.previousElementSibling.style.display =
+    "initial";
 
-    // on "désactive" la page active
-    document.querySelector(".active").classList.remove("active")
-     
-    // on incrémente pageActive
-     pageActive--
+  // on "désactive" la page active
+  document.querySelector(".active").classList.remove("active");
 
-     // on "active" le nouveau numéro
-     document.querySelector("#num"+pageActive).classList.add("active")
+  // on incrémente pageActive
+  pageActive--;
 
+  // on "active" le nouveau numéro
+  document.querySelector("#num" + pageActive).classList.add("active");
 }
 
 // Fonction qui permet d'imprimer une page
-function print_page(){
-    window.print();
-  }
+function print_page() {
+  window.print();
+}
+
+//popupcenter pour le datagrid
+function popupCenter(url, title, w, h) {
+  var left = screen.width / 2 - w / 2;
+  var top = screen.height / 2 - h / 2;
+  return window.open(
+    url,
+    title,
+    "toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=" +
+      w +
+      ", height=" +
+      h +
+      ", top=" +
+      top +
+      ", left=" +
+      left
+  );
+}
